b17b62e71f6900b841cd9ee440cb13efdb4a6969
hansumin52978 <gkstnals2626@naver.com>
2023-11-23 15:06:13 +0900

    # 4. 할 일 추가 기능 완료

--- src/components/TodoMain.js
+++ src/components/TodoMain.js
@@ -2,12 +2,17 @@ import React from 'react';
 import TodoItem from './TodoItem';
 import './scss/TodoMain.scss';
 
-const TodoMain = () => {
+const TodoMain = ({ todoList }) => {
   return (
     <ul className='todo-list'>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      {
+        todoList.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            item={todo}
+          />
+        )) // 배열내의 객체를 일괄적으로 지정하고 싶으면
+      }
     </ul>
   );
 };
