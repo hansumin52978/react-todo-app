f88e6795c3858e7b819a0d4cb439ed0ec78a55eb
hansumin52978 <gkstnals2626@naver.com>
2023-11-24 14:26:25 +0900

    TodoTemplate.js 수정

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -3,10 +3,9 @@ import TodoHeader from './TodoHeader';
 import TodoMain from './TodoMain';
 import TodoInput from './TodoInput';
 import './scss/TodoTemplate.scss';
-import { json } from 'react-router-dom';
 
 const TodoTemplate = () => {
-  // 서버에서 할 일 목록(json)을 요청(fetch)해서 받아와야 함. ->  나중에 하자
+  // 서버에 할 일 목록(json)을 요청(fetch)해서 받아와야 함.
   const API_BASE_URL = 'http://localhost:8181/api/todos';
 
   // todos 배열을 상태 관리
@@ -20,14 +19,14 @@ const TodoTemplate = () => {
 
   /*
     todoInput에게 todoText를 받아오는 함수
-    자식 컴포넌트가 부모컴포넌트에게 데이터를 전달할 때는
+    자식 컴포넌트가 부모컴포넌트에게 데이터를 전달할 때는 
     일반적인 props 사용이 불가능.
     부모 컴포넌트에서 함수를 선언(매개변수 꼭 선언) -> props로 함수를 전달
     자식 컴포넌트에서 전달받은 함수를 호출하면서 매개값으로 데이터를 전달.
   */
   const addTodo = (todoText) => {
     const newTodo = {
-      title: todoText, // 타이틀 값만 가지게
+      title: todoText,
     }; // 나중에 fetch를 이용해서 백엔드에 insert 요청 보내야됨.
 
     // todos.push(newTodo); (x) -> useState 변수는 setter로 변경
@@ -51,9 +50,9 @@ const TodoTemplate = () => {
 
   // 할 일 삭제 처리 함수
   const removeTodo = (id) => {
-    // 주어진 배열의 값들을 순화하여 조건에 맞는 요소들만 모아서 새로운 배열로 리턴.
+    // 주어진 배열의 값들을 순회하여 조건에 맞는 요소들만 모아서 새로운 배열로 리턴.
     // setTodos(todos.filter((todo) => todo.id !== id));
-    // fetch(API_BASE_URL + '/' + id, {
+
     fetch(`${API_BASE_URL}/${id}`, {
       method: 'DELETE',
     })
@@ -75,6 +74,7 @@ const TodoTemplate = () => {
     })
       .then((res) => res.json())
       .then((json) => setTodos(json.todos));
+
     /*
     const copyTodos = [...todos];
     for (let cTodo of copyTodos) {
@@ -84,10 +84,9 @@ const TodoTemplate = () => {
     }
     setTodos(copyTodos);
     
-    
     setTodos(
       todos.map((todo) =>
-      todo.id === id ? { ...todo, done: !todo.done } : todo
+        todo.id === id ? { ...todo, done: !todo.done } : todo
       )
     );
     */
